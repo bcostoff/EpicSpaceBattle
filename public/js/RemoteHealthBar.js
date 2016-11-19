c3f2a5541a361e4e2220c152d35006525af88c00
Beau Costoff <wbeaumont@rogers.com>
2016-11-18 23:33:58 -0500

    Conor Update

--- public/js/RemoteHealthBar.js
+++ public/js/RemoteHealthBar.js
@@ -21,7 +21,7 @@ HealthBar.prototype.mergeWithDefaultConfiguration = function(newConfig) {
         x: 0,
         y: 0,
         bg: {
-            color: '#651828'
+            color: '#000000'
         },
         bar: {
             color: '#FFD200'
