7c47ff1bca2d4c222e14fe6cbed369584a5af2c6
Beau Costoff <wbeaumont@rogers.com>
2016-11-20 13:25:36 -0500

    Updates

--- public/js/play.js
+++ public/js/play.js
@@ -272,7 +272,7 @@ function damageEnemy(e, l){
    console.log('Enemy Hit!')
   // if(Math.abs(laser.body.velocity.x) > 0 && Math.abs(laser.body.velocity.y) > 0) {  
 
-  //socket.emit('take damage', { damageType: 'laser', enemy: e.name, laser: l.name })
+  socket.emit('take damage', { damageType: 'laser', enemy: e.name, laser: l.name })
   l.kill();   
   // }
 }
