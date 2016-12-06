7c171d1faa35d2ded45461be3f158c4facaae134
smazing <smazingwebdev@gmail.com>
2016-12-06 15:00:06 -0500

    Mini Map

--- public/js/load.js
+++ public/js/load.js
@@ -13,6 +13,7 @@ var loadState = {
                 game.load.spritesheet('dude-alt-blue', 'assets/B2.png', 160, 160)
                 game.load.image('laser', 'assets/laser.png');
                 game.load.image('laser-alt', 'assets/laser2.png');
+                game.load.image('mini_map', 'assets/mini_map.png');
                 //game.load.image('rock', 'assets/rock.png');
                 //game.load.image("background", "images/jungle-bg-2.png");
 
--- public/js/play.js
+++ public/js/play.js
@@ -48,10 +48,13 @@ var playState = {
       // Start listening for events
       //setEventHandlers()
       onLobbyJoined();
+      createMiniMap();
 
 	},
 
 	update: function(){
+
+    updateUnitDots(player,enemies);
 
 		//-----------TEST ITEM---------//
     //game.physics.arcade.collide(player, rock, changeHealth, null, this);                
@@ -347,4 +350,64 @@ function crashPlayers(player, enemy){
 
 function isEven(n) {
    return n % 2 == 0;
+}
+
+
+function createMiniMap() {    
+  miniMapContainer = game.add.group();    
+  resolution = 2 / 100;    
+  if (game.world.width > 8000) {        
+    var renderWH = 8000;    
+  } else {       
+    var renderWH = game.world.width;    
+  }    
+  renderTexture = game.add.renderTexture(renderWH, renderWH);    
+  renderTexture.resolution = resolution;    
+  var cropRect = new Phaser.Rectangle(0, 0, 200, 200);    
+  renderTexture.crop = cropRect;    
+  var miniMapY = game.camera.view.height - (game.world.height * resolution);    
+  var miniMapUI = game.add.image(0, 0, 'mini_map');    
+  renderTexture.trueWidth = renderTexture.resolution * game.world.width;    
+  renderTexture.trueHeight = renderTexture.resolution * game.world.height;    
+  var cropRect = new Phaser.Rectangle(0, 0, renderTexture.trueWidth, renderTexture.trueHeight);    
+  renderTexture.crop = cropRect;    
+  var miniWidth = .075 * renderTexture.trueWidth;    
+  var miniHeight = miniMapY - (.06 * renderTexture.trueHeight);    
+  miniMap = game.add.sprite(miniWidth, miniHeight, renderTexture);    
+  var padding = .241 * renderTexture.trueHeight;    
+  miniMapUI.width = (renderTexture.trueWidth + padding);    
+  miniMapUI.height = (renderTexture.trueHeight + padding);    
+  miniMapUI.y = game.camera.view.height - miniMapUI.height;    
+  miniMapUI.fixedToCamera = true;    
+  miniMap.fixedToCamera = true;    
+  viewRect = game.add.graphics(0, 0);    
+  viewRect.lineStyle(1, 0xFFFFFF);    
+  viewRect.drawRect(miniMap.x, miniMap.y, game.camera.view.width * resolution, game.camera.view.height * resolution);    
+  unitDots = game.add.graphics(miniMap.x, miniMap.y);    
+  unitDots.fixedToCamera = true;    
+  var bg = game.add.graphics(0, 0);    
+  bg.beginFill(0x000000, 1);    
+  bg.drawRect(0, miniMapUI.y + (miniMapUI.height * .1), miniMapUI.width * .95, miniMapUI.height * .9);    
+  bg.fixedToCamera = true;    
+  var children = [bg, miniMap, unitDots, viewRect, miniMapUI];    
+  miniMapContainer.addMultiple(children);
+}
+
+
+function updateUnitDots(player,enemies) {    
+  unitDots.clear();            
+  var unitMiniX = player.player.x * renderTexture.resolution;        
+  var unitMiniY = player.player.y * renderTexture.resolution;    
+  var color = '0x6cf641';    
+  unitDots.beginFill(color);   
+  unitDots.drawEllipse(unitMiniX, unitMiniY, 2, 2);    
+  for(var i = 0; i < enemies.length; i++){
+    if(enemies[i].alive){
+      var eunitMiniX = enemies[i].player.x * renderTexture.resolution;        
+      var eunitMiniY = enemies[i].player.y * renderTexture.resolution;    
+      var color = '0x1f99f7';  
+      unitDots.beginFill(color);  
+      unitDots.drawEllipse(eunitMiniX, eunitMiniY, 2, 2);           
+    }
+  }       
 }
