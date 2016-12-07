6f9fbfba1fcecf0ff5bf4e7d4630f93cb6305402
smazing <smazingwebdev@gmail.com>
2016-12-07 08:56:27 -0500

    Scale Manager Changes

--- public/js/boot.js
+++ public/js/boot.js
@@ -1,6 +1,8 @@
 var bootState = {
 
 	create: function(){
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		//this.scale.scaleMode = Phaser.ScaleManager.RESIZE;
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		game.state.start('load');
 	}
