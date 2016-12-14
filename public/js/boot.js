27f798a511fcbbf8dcb25ce58b0d42e1e5241a18
smazing <smazingwebdev@gmail.com>
2016-12-14 11:38:23 -0500

    Add files via upload

--- public/js/boot.js
+++ public/js/boot.js
@@ -2,7 +2,7 @@ var bootState = {
 
 	create: function(){
 		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		game.scale.forceOrientation(true, false);
+		// game.scale.forceOrientation(true, false);
 		//this.scale.scaleMode = Phaser.ScaleManager.RESIZE;
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		game.state.start('load');
